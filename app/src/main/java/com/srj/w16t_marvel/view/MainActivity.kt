4dd75936c869160430a374568cf1fef7d73075b4
GitHubRawadJamal <106017266+GitHubRawadJamal@users.noreply.github.com>
2022-09-17 20:48:17 +0300

    add basics packages & implementations (#1)

--- app/src/main/java/com/srj/w16t_marvel/view/MainActivity.kt
+++ app/src/main/java/com/srj/w16t_marvel/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.srj.w16t_marvel
+package com.srj.w16t_marvel.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.srj.w16t_marvel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
